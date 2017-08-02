7abf67f82ca0063a736e50b6a98ea4e7a1f79cd4
Christian Diekmann <christian.diekmann@accenture.com>
2017-08-02 17:51:56 +0200

    Added imports and variables to list.components.html Small design adjustments to list.component.html

--- jsjuly2017/src/app/list/list.component.ts
+++ jsjuly2017/src/app/list/list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { JUMPSTARTER } from '../services/mock-jumpstarter';
+import { Jumpstarter } from '../services/jumpstarter';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+
+  jumpstarters: Jumpstarter[] =  JUMPSTARTER;
+  jumpstarter: Jumpstarter;
 
   constructor() { }
 
