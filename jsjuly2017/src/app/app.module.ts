0802562e17676e18baa15f17e0572039a9d64044
HomanSarwary <homan.sarwary@haw-hamburg.de>
2017-08-02 13:27:59 +0200

    Detailpage

--- jsjuly2017/src/app/app.module.ts
+++ jsjuly2017/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PlaygroundComponent } from './playground/playground.component';
 import { DetailpageComponent } from './detailpage/detailpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlaygroundComponent,
     DetailpageComponent
   ],
   imports: [
