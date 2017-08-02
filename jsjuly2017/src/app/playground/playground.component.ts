3cce5abb8ed08444f0803d3230bdb8322a45bfb9
Paszko, Jasmin <jasmin.paszko@accenture.com>
2017-08-02 16:05:20 +0200

    built in directives

--- jsjuly2017/src/app/playground/playground.component.ts
+++ jsjuly2017/src/app/playground/playground.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { JUMPSTARTER } from '../services/mock-jumpstarter';
+import { Jumpstarter } from '../services/jumpstarter';
+
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaygroundComponent implements OnInit {
 
+  jumpstarters: Jumpstarter[] =  JUMPSTARTER;
+  jumpstarter: Jumpstarter;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addJumpstarter(){
+    console.log(this.jumpstarter);
+    this.jumpstarters.push(this.jumpstarter);
   }
 
 }
