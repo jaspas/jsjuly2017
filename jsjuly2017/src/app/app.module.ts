33b9c11e1bf7433eff0bcfa6e36facb09a022e30
Schemel <corinna.schemel@accenture.com>
2017-08-02 13:28:04 +0200

    add

--- jsjuly2017/src/app/app.module.ts
+++ jsjuly2017/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PlaygroundComponent } from './playground/playground.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlaygroundComponent
+    PlaygroundComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule
