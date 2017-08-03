320289e9b4321d1ab94a6a451ad1b4db55ba4101
saber cherif <saber.cherif@accenture.com>
2017-08-03 16:08:25 +0200

    dynamic picture added

--- jsjuly2017/src/app/services/mock-jumpstarter.ts
+++ jsjuly2017/src/app/services/mock-jumpstarter.ts
@@ -18,7 +18,7 @@ export const JUMPSTARTER : Jumpstarter[] = [
       telephone: "+491755766634",
       email: "jasmin.paszko@accenture.com",
       jumpstart: "Java April 2012",
-      picture: "jasmin.jpg",
+      picture: "Degu.jpg",
       homeoffice: "Munich",
       country: "DE"
     }
