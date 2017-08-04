07fcccecd68ad570b611c5191f5b7b6a9d1eb349
DIR\leonid.abrahams <leonid.abrahams@accenture.com>
2017-08-04 10:52:57 +0200

    once again

---
+++ jsjuly2017/src/app/mailto.pipe.spec.ts
@@ -0,0 +1,10 @@
+import { MailtoPipe } from './mailto.pipe';
+
+describe('MailtoPipe', () => {
+  it('create an instance', () => {
+    const pipe = new MailtoPipe();
+    expect(pipe).toBeTruthy();
+  });
+
+  
+});
---
+++ jsjuly2017/src/app/mailto.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'mailto'
+})
+export class MailtoPipe implements PipeTransform {
+
+  transform(email: string): any {
+
+        return "mailto:" + email;
+  }
+  
+}
